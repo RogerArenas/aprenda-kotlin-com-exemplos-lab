2cb10ee81a0bc3d1971ea1e17d9767e07776f00e
Rogerio Arenas <rogerioarenas@hotmail.com>
2024-02-02 01:07:39 -0300

    feat: Refactor code for process challenger

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,36 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario ( val name: String, val email:String , val age:Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+    if(!inscritos.contains(usuario){
+         
+     inscritos.add(usuario)
+     println("${usuario.name} foi adicionado a lista")
+    }else {
+            println("${usuario.name} já está matriculado na formação $nome.")
+        }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+ 
+  val user01 = Usuario("RogerArenas", "rogerioarenas@gmail.com", 35)
+  val user02 = Usuario("RodrigoArenas", "rodrigoarenas@gmail.com", 09)
+
+  val conteudo01 = ConteudoEducacional("Desbravando Kotlin com Springboot", 10)
+  val conteudo02 = ConteudoEducacional("Logica de programacao", 100)
+
+  val formacao = Formacao("Ciência da Computação", listOf(conteudo01, conteudo02))
+
+  formacao.matricular(user01)
+  formacao.matricular(user02)
+  formacao.matricular(user01) 
+
+
 }
